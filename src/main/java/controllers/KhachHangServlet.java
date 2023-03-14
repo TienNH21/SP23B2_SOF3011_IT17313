56fa6e44bff8ef4ed49bd0bc2d554e3505617c21
HoangTien339 <tiennh21@fpt.edu.vn>
2023-03-14 14:23:59 +0700

    QLKH: Index

--- src/main/java/controllers/KhachHangServlet.java
+++ src/main/java/controllers/KhachHangServlet.java
@@ -3,8 +3,10 @@ package controllers;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import view_model.QLKhachHang;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet({
     "/khach-hang/index",    // GET
@@ -15,18 +17,29 @@ import java.io.IOException;
     "/khach-hang/update",   // POST
 })
 public class KhachHangServlet extends HttpServlet {
+    private ArrayList<QLKhachHang> list = new ArrayList<>();
+
     @Override
     protected void doGet(
         HttpServletRequest request,
         HttpServletResponse response
     ) throws ServletException, IOException {
-        this.create(request, response);
+        String uri = request.getRequestURI();
+        if (uri.contains("create")) {
+            this.create(request, response);
+        } else if (uri.contains("edit")) {
+            // edit
+        } else if (uri.contains("delete")) {
+            // delete
+        } else {
+            this.index(request, response);
+        }
     }
 
     @Override
     protected void doPost(
-            HttpServletRequest request,
-            HttpServletResponse response
+        HttpServletRequest request,
+        HttpServletResponse response
     ) throws ServletException, IOException {
         this.store(request, response);
     }
@@ -45,6 +58,15 @@ public class KhachHangServlet extends HttpServlet {
             HttpServletResponse response
     ) throws ServletException, IOException {
         request.getRequestDispatcher("/views/khach_hang/create.jsp")
+            .forward(request, response);
+    }
+
+    protected void index(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+        request.setAttribute("danhSachKH", list);
+        request.getRequestDispatcher("/views/khach_hang/index.jsp")
                 .forward(request, response);
     }
 }
